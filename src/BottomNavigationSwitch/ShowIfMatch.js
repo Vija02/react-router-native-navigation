6c1d4cb45a22415c04b89c992da57283dcce5d0f
Michael Salim <salim_michaelmi@yahoo.com>
2019-01-04 14:12:17 +0000

    refactor: move the code for blocking update into UpdateIfMatch

--- src/BottomNavigationSwitch/ShowIfMatch.js
+++ src/BottomNavigationSwitch/ShowIfMatch.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { View, StyleSheet } from 'react-native'
 
+import UpdateIfMatch from '../utils/UpdateIfMatch'
+
 const propTypes = {
 	match: PropTypes.bool,
 	lazy: PropTypes.bool,
@@ -18,11 +20,6 @@ class ShowIfMatch extends Component {
 		this.state = { rendered: false }
 	}
 
-	// This will prevent update calls to the component if it's not matched
-	shouldComponentUpdate(nextProps) {
-		return nextProps.match !== this.props.match
-	}
-
 	componentDidUpdate() {
 		if (this.props.match && !this.state.rendered) {
 			this.setState({ rendered: true })
@@ -34,7 +31,11 @@ class ShowIfMatch extends Component {
 		if (!match && this.props.lazy && !this.state.rendered) {
 			return null
 		}
-		return <View style={match ? styles.flexFull : styles.displayNone}>{this.props.children}</View>
+		return (
+			<UpdateIfMatch match={match}>
+				<View style={match ? styles.flexFull : styles.displayNone}>{this.props.children}</View>
+			</UpdateIfMatch>
+		)
 	}
 }
 
---
+++ src/utils/UpdateIfMatch.js
@@ -0,0 +1,25 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const propTypes = {
+	match: PropTypes.bool,
+}
+
+const defaultProps = {
+	match: false,
+}
+// We don't want the screen/page to keep updating even when nothing's happening there
+// This component just checks if there's changes and block it if there's none
+class UpdateIfMatch extends Component {
+	shouldComponentUpdate(nextProps) {
+		return nextProps.match !== this.props.match
+	}
+
+	render() {
+		return this.props.children
+	}
+}
+
+UpdateIfMatch.propTypes = propTypes
+UpdateIfMatch.defaultProps = defaultProps
+export default UpdateIfMatch
